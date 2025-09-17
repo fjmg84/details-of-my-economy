b94ab7a260480934eabc1d4fc863d26f3de3adc3
fjmg1984 <fjmiranda@tetraimpacts.com>
2025-09-16 20:18:09 -0500

    refactor: centralize route management and translate paths to Spanish

---
+++ src/utils/routes.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  DASHBOARD: "/dashboard/",
+  INGRESOS: "/dashboard/ingresos/",
+  GASTOS: "/dashboard/gastos/",
+  GRAFICOS: "/dashboard/graficos/",
+  ASISTENTE: "/dashboard/asistente/",
+  DOCUMENTACION: "/ayuda/",
+};
